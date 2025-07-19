d5dee423418bd43de19bdfb74f16a866852ff80b
Savo Shen <savo_shen@qq.com>
2025-07-19 10:41:23 +0800

    项目个人优化

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -25,7 +25,7 @@ defineProps({
   }
 })
 
-const title = ref('ruoyi-wms');
+const title = ref('威特');
 const settingsStore = useSettingsStore();
 const sideTheme = computed(() => settingsStore.sideTheme);
 </script>
