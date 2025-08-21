a1d4e967bd091d2fe58a0fef64d48b78c00cc617
Savo Shen <savo_shen@qq.com>
2025-08-21 11:38:39 +0800

    新增位置管理管理页面

---
+++ src/views/wms/basic/location/index.vue
@@ -0,0 +1,220 @@
+<template>
+  <div class="app-container">
+    <el-card>
+      <el-form :model="queryParams" ref="queryRef" :inline="true" label-width="68px">
+        <el-form-item label="位置名称" prop="locationName">
+          <el-input
+            v-model="queryParams.locationName"
+            placeholder="请输入位置名称"
+            clearable
+            @keyup.enter="handleQuery"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+          <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+
+    <el-card class="mt20">
+
+      <el-row :gutter="10" class="mb8" type="flex" justify="space-between">
+        <el-col :span="6"><span style="font-size: large">位置列表</span></el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="primary"
+            plain
+            icon="Plus"
+            @click="handleAdd"
+            v-hasPermi="['wms:location:edit']"
+          >新增</el-button>
+        </el-col>
+      </el-row>
+
+      <el-table v-loading="loading" :data="locationList" border class="mt20" empty-text="暂无位置">
+        <el-table-column label="位置编码" prop="locationCode" />
+        <el-table-column label="位置名称" prop="locationName" />
+        <el-table-column label="备注" prop="remark" width="360"/>
+        <el-table-column label="操作" align="right" class-name="small-padding fixed-width" width="180">
+          <template #default="scope">
+            <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['wms:location:edit']">修改</el-button>
+            <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['wms:location:edit']">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+    </el-card>
+    <!-- 添加或修改商品位置对话框 -->
+    <el-drawer :title="title" v-model="open" size="50%" append-to-body>
+      <el-form ref="locationRef" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="位置编码" prop="locationCode">
+          <el-input v-model="form.locationCode" placeholder="请输入位置编码" />
+        </el-form-item>
+        <el-form-item label="位置名称" prop="locationName">
+          <el-input v-model="form.locationName" placeholder="请输入位置名称" />
+        </el-form-item>
+        <el-form-item label="备注" prop="remark">
+          <el-input v-model="form.remark" placeholder="请输入备注" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+    </el-drawer>
+  </div>
+</template>
+
+<script setup name="Location">
+import { listLocationNoPage, listLocation, getLocation, addLocation, updateLocation, delLocation } from "@/api/wms/location";
+import {ElMessageBox} from "element-plus";
+import {useWmsStore} from '@/store/modules/wms'
+
+const { proxy } = getCurrentInstance();
+
+const locationList = ref([]);
+const open = ref(false);
+const buttonLoading = ref(false);
+const loading = ref(true);
+const ids = ref([]);
+const total = ref(0);
+const title = ref("");
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    locationName: undefined,
+    locationCode: undefined,
+    remark: undefined
+  },
+  rules: {
+    locationName: [
+      { required: true, message: "位置名称不能为空", trigger: "blur" }
+    ],
+    locationCode: [
+      { required: true, message: "位置编号不能为空", trigger: "blur" }
+    ]
+  }
+});
+
+const { queryParams, form, rules } = toRefs(data);
+
+/** 查询商品位置列表 */
+async function getList() {
+  loading.value = true;
+  const store = useWmsStore()
+  await store.getLocationList()
+
+  let list = [...store.locationList]
+  if (queryParams.value.locationName) {
+    list = list.filter(it => it.locationName === queryParams.value.locationName)
+  }
+  locationList.value = list;
+  loading.value = false;
+}
+
+// 取消按钮
+function cancel() {
+  open.value = false;
+  reset();
+}
+
+// 表单重置
+function reset() {
+  form.value = {
+    id: null,
+    locationCode: null,
+    locationName: null,
+    remark: null,
+    createBy: null,
+    createTime: null,
+    updateBy: null,
+    updateTime: null
+  };
+  proxy.resetForm("locationRef");
+}
+
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+}
+
+/** 重置按钮操作 */
+function resetQuery() {
+  proxy.resetForm("queryRef");
+  handleQuery();
+}
+
+/** 新增按钮操作 */
+function handleAdd() {
+  reset();
+  open.value = true;
+  title.value = "添加商品位置";
+}
+
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  reset();
+  const _id = row.id || ids.value
+  getLocation(_id).then(response => {
+    form.value = response.data;
+    open.value = true;
+    title.value = "修改商品位置";
+  });
+}
+
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["locationRef"].validate(valid => {
+    if (valid) {
+      buttonLoading.value = true;
+      if (form.value.id != null) {
+        updateLocation(form.value).then(response => {
+          proxy.$modal.msgSuccess("修改成功");
+          open.value = false;
+          getList();
+        }).finally(() => {
+          buttonLoading.value = false;
+        });
+      } else {
+        addLocation(form.value).then(response => {
+          proxy.$modal.msgSuccess("新增成功");
+          open.value = false;
+          getList();
+        }).finally(() => {
+          buttonLoading.value = false;
+        });
+      }
+    }
+  });
+}
+
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const _ids = row.id || ids.value;
+  proxy.$modal.confirm('确认删除位置【' + row.locationName + '】吗？').then(function() {
+    return delLocation(_ids);
+  }).then(() => {
+    loading.value = true;
+    getList();
+    proxy.$modal.msgSuccess("删除成功");
+  }).finally(() => {
+    loading.value = false;
+  });
+}
+
+/** 导出按钮操作 */
+function handleExport() {
+  proxy.download('wms/location/export', {
+    ...queryParams.value
+  }, `location_${new Date().getTime()}.xlsx`)
+}
+
+getList();
+</script>
